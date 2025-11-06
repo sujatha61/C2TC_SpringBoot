966b8c3f46ff05ebedfd67f8b56c28959162af1b
sujatha61 <sujatha6153@gmail.com>
2025-11-06 22:00:58 +0530

    springboot

---
+++ spring-boot-web-app/src/main/java/com/tnsif/spring_boot_web_app/SpringBootWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.spring_boot_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppApplication.class, args);
+	}
+
+}
---
+++ spring-boot-web-app/src/test/java/com/tnsif/spring_boot_web_app/SpringBootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.spring_boot_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
